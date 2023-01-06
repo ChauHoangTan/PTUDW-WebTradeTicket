75137ce9fff12c0bcc61c2b582856608f8d7a899
ChauHoangTan <20127621@student.hcmus.edu.vn>
2023-01-07 01:27:19 +0700

    update

--- Final/controllers/loginController.js
+++ Final/controllers/loginController.js
@@ -1,14 +1,19 @@
 const controller = {};
-const express = require('express');
 const model = require('../models');
+const Title = "Đăng nhập"
 
-controller.showlogin = (req,res) => {
-    res.render('Log-in',{layout:'layout2'});
+controller.showlogin = async (req,res) => {
+    
+   
+
+    res.locals.test = ["list1","list2"];
+
+    res.render('Log-in',{layout:'layout2', Title: Title });
 }
 
 controller.checkAccount = async (req,res)=>{
     let user = await model.Khach_Hang.findAll();
-    res.local.userList = user;
+    res.locals.userList = user;
 }
 
 module.exports = controller;
--- Final/routes/loginRoute.js
+++ Final/routes/loginRoute.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const route = express.Router();
 const controller = require('../controllers/loginController');
+const models = require('../models');
+
 
 route.get('/',(req,res)=>{
     controller.showlogin(req,res);
 })
 
-route.post('/view',(req,res) => {
+route.post('/',(req,res) => {
     controller.checkAccount(req,res);
 });
 
