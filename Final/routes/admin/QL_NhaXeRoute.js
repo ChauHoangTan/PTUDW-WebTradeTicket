fb52e2498d7ac504c3d2f9d8a7f20aed8ad9ad5a
Nguyễn Huy Hoàn <huyhoan652002@gmail.com>
2023-01-08 19:22:59 +0700

    Add files via upload

--- Final/routes/admin/QL_NhaXeRoute.js
+++ Final/routes/admin/QL_NhaXeRoute.js
@@ -14,15 +14,5 @@ route.get('/Remove/:id', (req, res) => {
     controller.Remove(req,res);
 });
 
-route.post('/Edit/:id', (req, res) => {
-    controller.Edit(req,res);
-});
-
-// add new nha xe
-route.post('/Add', (req, res) => {
-    controller.Add(req,res);
-});
-
-
 
 module.exports = route;
